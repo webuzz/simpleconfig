289c856f6f49702758faeb6e0a47d24ecce8481d
Zhou Renjian <zhourenjian@gmail.com>
2015-09-21 23:37:32 +0800

    Add configuration agent for non-Java server configuration monitoring

---
+++ agent/im/webuzz/config/agent/ConfigAgent.java
@@ -0,0 +1,30 @@
+package im.webuzz.config.agent;
+
+import im.webuzz.config.Config;
+
+public class ConfigAgent {
+
+	public static boolean agentRunning = true;
+	
+	public static long agentSleepInterval = 10000;
+	
+	public static void main(String[] args) {
+		if (args == null || args.length < 0 || args[0] == null || args[0].length() < 0) {
+			System.out.println("Usage: java -cp . " + ConfigAgent.class.getName() + " <config.ini>");
+			return;
+		}
+		
+		Config.initialize(args[0]);
+		Config.registerUpdatingListener(ConfigAgent.class);
+		System.out.println("Config agent started.");
+		while (agentRunning) {
+			try {
+				Thread.sleep(agentSleepInterval);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+		System.out.println("Config agent stopped.");
+	}
+
+}
